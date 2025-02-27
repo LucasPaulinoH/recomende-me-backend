d919528d6eb3706288a526e6cf03f5b2c0f11d3e
Lucas Paulino <lucaspaulinoh@hotmail.com>
2025-02-27 09:30:16 -0300

    FEAT: Spring Boot project added

---
+++ src/test/java/com/paulino/recomendeme_challenge/RecomendemeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulino.recomendeme_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendemeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
